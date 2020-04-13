2d373da76b8560c4d39d3687bd2d33e63b429d6b
sxxgrc <sg62@rice.edu>
2020-04-12 23:35:17 -0500

    Initialization of DE component and main file.

---
+++ DecayDE.py
@@ -0,0 +1,34 @@
+"""
+An implementation of the Decay of Differential Expression component of the 
+GeneSurrounder algorithm.
+
+Takes in a network of putative genes, along with their phenotype labels.
+TODO: Talk about other inputs lol.
+"""
+# Import statements.
+
+# Global variables for stuff only computed once.
+
+# Helper functions for each step.
+def computeDistMat():
+    # TODO This is step 1
+    return
+
+def computeG():
+    # TODO This is step 2 Also change the function name
+    return
+
+def computeD():
+    # TODO This is step 3 Again change name plz
+    return
+
+def computeDistD():
+    # TODO This is step 4
+    return
+
+# Main function for final result.
+def decayDE():
+    # TODO This should call above and compute final pi(r)
+    # Need to think about how we want to do this. Should decayDE() compute over
+    # all r values? This would be ideal so as to only do steps 1 2 and 4 once.
+    return
---
+++ GeneSurrounder.py
@@ -0,0 +1,8 @@
+"""
+Main program for the GeneSurrounder algorithm.
+"""
+
+# Imports.
+
+# TODO: Take in command line arguments for the files and call decayDE and SI to
+# compute full result.
